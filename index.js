7a5e4a7da03a46d7adc5aeb86f6b6bbbb0f92c2d
Blen-Hailu <belaki2@gmail.com>
2022-11-16 13:37:04 +0200

    publish

--- index.js
+++ index.js
@@ -7,4 +7,27 @@ app.get("/hello",(request, response) => {
   response.header({"Access-Control-Allow-Origin": "*"});
   response.json({ data: "Hello World!"})
   })
-})
+
+//copied from backend test 
+  const postgres = require('postgres');
+require('dotenv').config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PROJECT_NAME } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${PROJECT_NAME}`;
+
+const sql = postgres(URL, { ssl: 'require' });
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+getPgVersion();
+
+async function getData(){
+const result = await sql `SELECT *From playing_with_neon
+  console.log(result);
+ }
+getData();
+async function updateColumnDAta(){
+
+}
